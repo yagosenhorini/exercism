76aa1d378ae3c59fa61478cdd8d5a420328735e4
yagosenhorini <yagosenhorini30@gmail.com>
2017-12-01 09:31:48 -0200

    Treinamento Exercism

--- ecmascript/binary/binary.js
+++ ecmascript/binary/binary.js
@@ -4,14 +4,27 @@ class Binary {
     }
 
     toDecimal() {
-        let bin = this.binary;
-        var result = parseInt(bin, 2);
+            var char = 'abcdefghijklmnopqrstuvwxyz23456789';
+            var bin = this.binary;
+            var result = parseInt(bin, 2);
+            for (let i = 0; i < bin.length; i++) {
+                let a = bin[i];
+                if (char.indexOf(a) >= 0) {
+                    return 0;
+                }
+            }
+            return result;
+
+
+        }
+        /*Or, do this fucking if. 
         if (bin.match(/[a-zA-Z2-9]/g)) {
             return 0;
-        } else {
-            return result;
-        }
-    }
+         } else {
+             return result;
+        }*/
+
+
 }
 
 export default Binary;
